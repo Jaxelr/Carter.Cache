4236b49064cfc56edd0c1006d69a4b0dcf1cd46c
Jaxel Rojas <jaxelr@gmail.com>
2020-08-26 14:13:32 -0400

    tests: added default key and store fixtures

---
+++ tests/Carter.Cache.Tests/Unit/Stores/DefaultMemoryStoreFixtures.cs
@@ -0,0 +1,22 @@
+﻿using Carter.Cache.Stores;
+using Xunit;
+
+namespace Carter.Cache.Tests.Unit.Stores
+{
+    public class DefaultMemoryStoreFixtures
+    {
+        [Fact]
+        public void IMemory_cache_empty_get()
+        {
+            //Arrange
+            var cache = new DefaultMemoryStore();
+
+            //Act
+            bool found = cache.TryGetValue(string.Empty, out var response);
+
+            //Assert
+            Assert.False(found);
+            Assert.Null(response);
+        }
+    }
+}
