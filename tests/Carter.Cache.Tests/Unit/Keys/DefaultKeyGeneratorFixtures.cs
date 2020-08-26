5d5e8d8b4550af350d21d3c7f5f0b1404c394001
Jaxel Rojas <jaxelr@gmail.com>
2020-08-26 14:13:32 -0400

    tests: added default key and store fixtures

---
+++ tests/Carter.Cache.Tests/Unit/Keys/DefaultKeyGeneratorFixtures.cs
@@ -0,0 +1,21 @@
+﻿using Carter.Cache.Keys;
+using Xunit;
+
+namespace Carter.Cache.Tests.Unit.Keys
+{
+    public class DefaultKeyGeneratorFixtures
+    {
+        [Fact]
+        public void Default_keyed_null()
+        {
+            //Arrange
+            var keyGen = new DefaultKeyGenerator();
+
+            //Act
+            string key = keyGen.Get(null);
+
+            //Assert
+            Assert.Empty(key);
+        }
+    }
+}
