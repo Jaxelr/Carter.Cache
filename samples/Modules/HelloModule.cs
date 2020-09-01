fa1f5673b30d500059075f7222ec3c09d7caff95
Jaxel Rojas <jaxelr@gmail.com>
2020-09-01 15:47:38 -0400

    chore: updated sample with the newest api

--- samples/Modules/HelloModule.cs
+++ samples/Modules/HelloModule.cs
@@ -16,7 +16,35 @@ namespace Sample.Carter.Cache.Application.Modules
             {
                 try
                 {
-                    ctx.Cacheable(10);
+                    ctx.AsCacheable(10);
+
+                    string name = ctx.Request.RouteValues.As<string>("name");
+
+                    string response = repository.SayHello(name);
+
+                    if (response == null)
+                    {
+                        ctx.Response.StatusCode = 204;
+                        return;
+                    }
+
+                    ctx.Response.StatusCode = 200;
+                    await ctx.Response.Negotiate(response)
+                        .ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    ctx.Response.StatusCode = 500;
+                    await ctx.Response.Negotiate(new FailedResponse(ex))
+                        .ConfigureAwait(false);
+                }
+            });
+
+            Get<GetHello>("/Hello2/{name}", async (ctx) =>
+            {
+                try
+                {
+                    ctx.AsCacheable(15);
 
                     string name = ctx.Request.RouteValues.As<string>("name");
 
