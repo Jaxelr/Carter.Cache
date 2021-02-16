5a1cbb6d9aea12ad61deac1b0bd3c88db114340c
Jaxel Rojas <jaxelr@gmail.com>
2021-02-16 14:02:51 -0400

    tests: added application builder extension test fixture

---
+++ tests/Carter.Cache.Tests/Unit/Extensions/ApplicationBuilderExtensionsFixtures.cs
@@ -0,0 +1,25 @@
+﻿using FakeItEasy;
+using Microsoft.AspNetCore.Builder;
+using Xunit;
+
+namespace Carter.Cache.Tests.Unit.Extensions
+{
+    public class ApplicationBuilderExtensionsFixtures
+    {
+        [Fact]
+        public void Application_builder()
+        {
+            //Arrange
+            var builder = A.Fake<IApplicationBuilder>();
+
+            A.CallTo(() => builder.ApplicationServices.GetService(typeof(CachingOption)))
+                .Returns(new CachingOption());
+
+            //Act
+            builder.UseCarterCaching();
+
+            //Assert
+            Assert.NotNull(builder.ServerFeatures.Get<CarterCachingService>());
+        }
+    }
+}
