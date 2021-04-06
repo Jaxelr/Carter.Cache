dbe4f3bb245dd951e58ec0ae54ff9244467db35a
Jaxel Rojas <jaxelr@gmail.com>
2021-04-05 21:27:05 -0400

    feat: initial prj template

---
+++ src/Carter.Cache.Memcached/MemcachedStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Carter.Cache.Stores;
+
+namespace Carter.Cache.Memcached
+{
+    public class MemcachedStore : ICacheStore
+    {
+        public void Remove(string key) => throw new NotImplementedException();
+        public void Set(string key, CachedResponse response, TimeSpan expiration) => throw new NotImplementedException();
+        public bool TryGetValue(string key, out CachedResponse cachedResponse) => throw new NotImplementedException();
+    }
+}
