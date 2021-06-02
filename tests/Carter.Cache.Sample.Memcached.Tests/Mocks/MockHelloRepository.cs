c5c7f48272d0d92dd722f26d72ff5f5ce0bd0b46
Jaxel Rojas <jaxelr@gmail.com>
2021-06-02 12:29:34 -0400

    tests: added test for memcached sample app

---
+++ tests/Carter.Cache.Sample.Memcached.Tests/Mocks/MockHelloRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Sample.Carter.Cache.Memcached.Application.Repository;
+
+namespace Carter.Cache.Sample.Tests.Mocks
+{
+    public class MockHelloRepository : IHelloRepository
+    {
+        public string SayHello(string name) => $"Hello world, your name is {name} the hour cached is {DateTime.Now}";
+    }
+}
