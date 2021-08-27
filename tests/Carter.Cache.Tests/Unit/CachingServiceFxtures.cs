77f107b667bb1de5fdcbbdf2deeb6610f5fe13c9
Jaxel Rojas <jaxelr@gmail.com>
2021-08-27 09:35:22 -0400

    test: added test which validates etag scenario

--- tests/Carter.Cache.Tests/Unit/CachingServiceFxtures.cs
+++ tests/Carter.Cache.Tests/Unit/CachingServiceFxtures.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Text;
 using System.Threading.Tasks;
 using FakeItEasy;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
 using Xunit;
 
 namespace Carter.Cache.Tests.Unit
@@ -103,6 +105,53 @@ namespace Carter.Cache.Tests.Unit
 
             //Assert
             Assert.True(cacheHit);
+        }
+
+        [Fact]
+        public async Task Service_set_cache_default_with_request_body()
+        {
+            //Arrange
+            const string scheme = "http";
+            const string host = "localhost";
+            const int port = 80;
+            const string path = "/hello";
+            byte[] body = Encoding.UTF8.GetBytes("{\"Response\": \"Hello world\"}");
+            const int elapsedSeconds = 3;
+
+            var fakeSpan = TimeSpan.FromSeconds(elapsedSeconds);
+            var ctx = A.Fake<HttpContext>();
+            var option = A.Fake<CachingOption>();
+            var response = A.Fake<HttpResponse>();
+            var service = A.Fake<CarterCachingService>();
+            var request = A.Fake<HttpRequest>();
+            var headers = A.Fake<IHeaderDictionary>();
+            var props = A.Fake<CachingProperty>();
+
+            props.Expiration = TimeSpan.FromSeconds(elapsedSeconds);
+
+            A.CallTo(() => ctx.Features.Get<CachingProperty>()).Returns(props);
+            A.CallTo(() => ctx.Request).Returns(request);
+            A.CallTo(() => ctx.Response).Returns(response);
+
+            A.CallTo(() => request.Scheme).Returns(scheme);
+            A.CallTo(() => request.Host).Returns(new HostString(host, port));
+            A.CallTo(() => request.Path).Returns(path);
+
+            A.CallTo(() => response.Headers).Returns(headers);
+            A.CallTo(() => response.StatusCode).Returns(StatusCodes.Status200OK);
+
+            A.CallTo(() => response.Headers.ContainsKey(HeaderNames.ETag)).Returns(true);
+            A.CallTo(() => response.Headers[HeaderNames.ETag]).Returns(ctx.CalculateChecksum(body));
+
+            var cachedResponse = new CachedResponse(ctx, body);
+
+            //Act
+            await service.SetCache(ctx, cachedResponse, option);
+            bool cacheHit = await service.CheckCache(ctx, option);
+
+            //Assert
+            Assert.True(cacheHit);
+            Assert.True(ctx.Response.Headers.ContainsKey(HeaderNames.ETag));
         }
     }
 }
