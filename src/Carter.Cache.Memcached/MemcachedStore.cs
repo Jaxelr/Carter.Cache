ab48b758de841335a5d0931e4c193124e34eff60
Jaxel Rojas <jaxelr@gmail.com>
2021-12-22 07:25:20 -0400

    refactor: simplified namespace

--- src/Carter.Cache.Memcached/MemcachedStore.cs
+++ src/Carter.Cache.Memcached/MemcachedStore.cs
@@ -3,58 +3,57 @@ using Carter.Cache.Stores;
 using Enyim.Caching;
 using Enyim.Caching.Memcached;
 
-namespace Carter.Cache.Memcached
+namespace Carter.Cache.Memcached;
+
+public class MemcachedStore : ICacheStore
 {
-    public class MemcachedStore : ICacheStore, IDisposable
+    private readonly IMemcachedClient client;
+
+    public MemcachedStore(IMemcachedClient client) => this.client = client;
+
+    public void Dispose() => client.Dispose();
+
+    /// <summary>
+    /// Remove the element with the key provided.
+    /// </summary>
+    /// <param name="key"></param>
+    public void Remove(string key) => client.Remove(key);
+
+    /// <summary>
+    /// Store the CachedResponse object and assign the key provided for the duration included.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="response"></param>
+    /// <param name="expiration"></param>
+    public void Set(string key, CachedResponse response, TimeSpan expiration)
     {
-        private readonly IMemcachedClient client;
-
-        public MemcachedStore(IMemcachedClient client) => this.client = client;
-
-        public void Dispose() => client.Dispose();
-
-        /// <summary>
-        /// Remove the element with the key provided.
-        /// </summary>
-        /// <param name="key"></param>
-        public void Remove(string key) => client.Remove(key);
-
-        /// <summary>
-        /// Store the CachedResponse object and assign the key provided for the duration included.
-        /// </summary>
-        /// <param name="key"></param>
-        /// <param name="response"></param>
-        /// <param name="expiration"></param>
-        public void Set(string key, CachedResponse response, TimeSpan expiration)
+        if (string.IsNullOrEmpty(key))
         {
-            if (string.IsNullOrEmpty(key))
-            {
-                return;
-            }
-
-            if (expiration.TotalSeconds > 0)
-            {
-                client.Store(StoreMode.Set, key, response, expiration);
-            }
+            return;
         }
 
-        /// <summary>
-        /// Try and get the value from the store and return it.
-        /// </summary>
-        /// <param name="key"></param>
-        /// <param name="response"></param>
-        /// <returns>True if the value exists, false if not</returns>
-        public bool TryGetValue(string key, out CachedResponse response)
+        if (expiration.TotalSeconds > 0)
         {
-            response = null;
-
-            if (client.TryGet(key, out CachedResponse value))
-            {
-                response = value;
-                return true;
-            }
-
-            return false;
+            client.Store(StoreMode.Set, key, response, expiration);
         }
+    }
+
+    /// <summary>
+    /// Try and get the value from the store and return it.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="response"></param>
+    /// <returns>True if the value exists, false if not</returns>
+    public bool TryGetValue(string key, out CachedResponse response)
+    {
+        response = null;
+
+        if (client.TryGet(key, out CachedResponse value))
+        {
+            response = value;
+            return true;
+        }
+
+        return false;
     }
 }
--- src/Carter.Cache.Redis/RedisStore.cs
+++ src/Carter.Cache.Redis/RedisStore.cs
@@ -3,86 +3,85 @@ using Carter.Cache.Stores;
 using Newtonsoft.Json;
 using StackExchange.Redis;
 
-namespace Carter.Cache.Redis
+namespace Carter.Cache.Redis;
+
+public class RedisStore : ICacheStore
 {
-    public class RedisStore : ICacheStore, IDisposable
+    private readonly ConnectionMultiplexer redis;
+
+    public RedisStore(ConfigurationOptions options)
     {
-        private readonly ConnectionMultiplexer redis;
+        redis = ConnectionMultiplexer.Connect(options);
+    }
 
-        public RedisStore(ConfigurationOptions options)
+    public RedisStore(string configuration)
+    {
+        redis = ConnectionMultiplexer.Connect(configuration);
+    }
+
+    public void Dispose() => redis.Dispose();
+
+    /// <summary>
+    /// Remove the element with the key provided.
+    /// </summary>
+    /// <param name="key"></param>
+    public void Remove(string key)
+    {
+        var cache = redis.GetDatabase();
+        cache.KeyDelete(key);
+    }
+
+    /// <summary>
+    /// Store the CachedResponse object and assign the key provided for the duration included.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="response"></param>
+    /// <param name="expiration"></param>
+    public void Set(string key, CachedResponse response, TimeSpan expiration)
+    {
+        if (string.IsNullOrEmpty(key))
         {
-            redis = ConnectionMultiplexer.Connect(options);
+            return;
         }
 
-        public RedisStore(string configuration)
-        {
-            redis = ConnectionMultiplexer.Connect(configuration);
-        }
-
-        public void Dispose() => redis.Dispose();
-
-        /// <summary>
-        /// Remove the element with the key provided.
-        /// </summary>
-        /// <param name="key"></param>
-        public void Remove(string key)
+        if (expiration.TotalSeconds > 0)
         {
             var cache = redis.GetDatabase();
-            cache.KeyDelete(key);
-        }
 
-        /// <summary>
-        /// Store the CachedResponse object and assign the key provided for the duration included.
-        /// </summary>
-        /// <param name="key"></param>
-        /// <param name="response"></param>
-        /// <param name="expiration"></param>
-        public void Set(string key, CachedResponse response, TimeSpan expiration)
-        {
-            if (string.IsNullOrEmpty(key))
+            bool ack = cache.StringSet(key, JsonConvert.SerializeObject(response), expiry: expiration);
+
+            if (!ack)
             {
-                return;
-            }
-
-            if (expiration.TotalSeconds > 0)
-            {
-                var cache = redis.GetDatabase();
-
-                bool ack = cache.StringSet(key, JsonConvert.SerializeObject(response), expiry: expiration);
-
-                if (!ack)
-                {
-                    throw new Exception($"Could not complete operation of Set, using redis configuration: {redis.Configuration}");
-                }
+                throw new Exception($"Could not complete operation of Set, using redis configuration: {redis.Configuration}");
             }
         }
+    }
 
-        /// <summary>
-        /// Try and get the value from the store and return it.
-        /// </summary>
-        /// <param name="key"></param>
-        /// <param name="response"></param>
-        /// <returns>True if the value exists, false if not</returns>
-        public bool TryGetValue(string key, out CachedResponse response)
+    /// <summary>
+    /// Try and get the value from the store and return it.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="response"></param>
+    /// <returns>True if the value exists, false if not</returns>
+    public bool TryGetValue(string key, out CachedResponse response)
+    {
+        response = null;
+
+        if (string.IsNullOrEmpty(key))
         {
-            response = null;
-
-            if (string.IsNullOrEmpty(key))
-            {
-                return false;
-            }
-
-            var cache = redis.GetDatabase();
-
-            var result = cache.StringGet(key);
-
-            if (result.HasValue)
-            {
-                response = JsonConvert.DeserializeObject<CachedResponse>(result);
-                return true;
-            }
-
             return false;
         }
+
+        var cache = redis.GetDatabase();
+
+        var result = cache.StringGet(key);
+
+        if (result.HasValue)
+        {
+            response = JsonConvert.DeserializeObject<CachedResponse>(result);
+            return true;
+        }
+
+        return false;
     }
 }
